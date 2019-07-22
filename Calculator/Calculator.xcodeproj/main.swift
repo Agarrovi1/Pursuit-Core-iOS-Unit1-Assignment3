dc88396e3270c3fc311cb3b06614610e46f50aff
Agarrovi1 <52180618+Agarrovi1@users.noreply.github.com>
2019-07-21 22:41:15 -0400

    put in filter/map/reduce functions for calculator

--- Calculator/Calculator.xcodeproj/main.swift
+++ Calculator/Calculator.xcodeproj/main.swift
@@ -163,13 +163,188 @@ func randomGame() {
 }
 
 func SimpleOrGame() -> Int {
-    print("For simple calculations type in 1, \nFor an operation guessing game type in 2: ")
+    print("For simple calculations type in 1, \nFor an operation guessing game type in 2 \nFor more complex calculations type in 3: ")
     let input = readLine()
     var value = Int()
     if let unwrap = input?.first {
         if let integerVers = Int("\(unwrap)") {
             value = integerVers
         }
+    }
+    return value
+}
+
+func askForNums() -> [Int] {
+    print("please enter the numbers you wish to change separated by a comma ex 1,2,3: ")
+    var arrayOfInts = [Int]()
+    let array = readLine()?
+        .split {$0 == ","}
+        .map (String.init)
+    if let unwrap = array {
+        for a in unwrap {
+            if let unwrapped = Int(a) {
+                arrayOfInts.append(unwrapped)
+            }
+        }
+    }
+    return arrayOfInts
+}
+
+func askForCrocs() -> String {
+    print("please enter < or > for how you want to filter your numbers: ")
+    let input = readLine()
+    var ans = String()
+    if let justTheFirstChara = input?.first {
+        ans = "\(justTheFirstChara)"
+    }
+    return ans
+}
+
+func askForOperands() -> String {
+    print("please enter an operand for how you want to change your numbers: ")
+    let input = readLine()
+    var ans = String()
+    if let justTheFirstChara = input?.first {
+        ans = "\(justTheFirstChara)"
+    }
+    return ans
+}
+
+func onlySumAndProduce() -> String {
+    print("please enter an either + or * to reduce numbers by: ")
+    let input = readLine()
+    var ans = String()
+    if let justTheFirstChara = input?.first {
+        ans = "\(justTheFirstChara)"
+    }
+    return ans
+}
+
+func secondNum() -> Int {
+    print("Now enter the num you want to compare to (filter) or change by (map) or start at (reduce): ")
+    let input = readLine()
+    var ans = Int()
+    if let unwrap = input?.first {
+        if let intVers = Int("\(unwrap)") {
+            ans = intVers
+        }
+    }
+    return ans
+}
+
+func myFilter(inputArray: [Int], filter: (Int) -> Bool) -> [Int] {
+    let newArray = inputArray.filter(filter)
+    return newArray
+}
+
+func wholeFilter() -> [Int] {
+    let nums = askForNums()
+    let greaterOrLess = askForCrocs()
+    let compareTo = secondNum()
+    var ans = [Int]()
+    switch greaterOrLess {
+    case ">":
+        ans = myFilter(inputArray: nums, filter: {(a:Int) -> Bool in
+            return a > compareTo
+        })
+    case "<":
+        ans = myFilter(inputArray: nums, filter: {(a:Int) -> Bool in
+            return a < compareTo
+        })
+    default:
+        print("Invalid input")
+    }
+    return ans
+}
+
+//let sample = [1,2,3,4,5]
+//let test = myFilter(inputArray: sample, filter: {(a: Int) -> Bool in
+//    return a > 3})
+//print(test)
+
+func myMap(inputArray: [Int], closure: (Int) -> Int) -> [Int] {
+    var newArray = inputArray.map(closure)
+    return newArray
+}
+
+func wholeMap() -> [Int] {
+    let nums = askForNums()
+    let operand = askForOperands()
+    let byThisNum = secondNum()
+    var ans = [Int]()
+    switch operand {
+    case "+":
+        ans = myMap(inputArray: nums, closure: {(a:Int) -> Int in
+            return a + byThisNum
+        })
+    case "*":
+        ans = myMap(inputArray: nums, closure: {(a:Int) -> Int in
+            return a * byThisNum
+        })
+    case "-":
+        ans = myMap(inputArray: nums, closure: {(a:Int) -> Int in
+            return a - byThisNum
+        })
+    case "/":
+        if byThisNum != 0 {
+            ans = myMap(inputArray: nums, closure: {(a:Int) -> Int in
+                return a / byThisNum
+            })
+        } else {
+            print("cannot divide by zero")
+        }
+    default:
+        print("invalid input")
+    }
+    return ans
+}
+
+func myReduce(inputArray: [Int],startingPoint: Int, closure: (Int,Int) -> Int) -> Int {
+    return inputArray.reduce(startingPoint, closure)
+}
+
+func wholeReduce() -> Int {
+    let nums = askForNums()
+    let operand = onlySumAndProduce()
+    let start = secondNum()
+    var ans = Int()
+    switch operand {
+    case "*":
+        ans = myReduce(inputArray: nums, startingPoint: start, closure: {(a,b) -> Int in
+            return a * b
+        })
+    case "+":
+        ans = myReduce(inputArray: nums, startingPoint: start, closure: {(a,b) -> Int in
+            return a + b
+        })
+    default:
+        print("invalid input")
+    }
+    return ans
+}
+
+func mapsFilterReduceOhMy(string: String) {
+    switch string {
+    case "filter":
+        let answer1 = wholeFilter()
+        print(answer1)
+    case "map":
+        let answer2 = wholeMap()
+        print(answer2)
+    case "reduce":
+        let answer3 = wholeReduce()
+        print(answer3)
+    default:
+        print("invalid input")
+    }
+}
+
+func complex() -> String {
+    print("Type in the specific keywords for methods- filter, map, or reduce: ")
+    let input = readLine()
+    var value = String()
+    if let unwrap = input {
+        value = unwrap
     }
     return value
 }
@@ -181,6 +356,9 @@ func Calculator() {
         simpleCalculations()
     case 2:
         randomGame()
+    case 3:
+        let keyword = complex()
+        mapsFilterReduceOhMy(string: keyword)
     default:
         print("not a valid input")
     }
